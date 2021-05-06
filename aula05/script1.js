008ac43d864fd44459de0354a8f6320c56618b62
SophiaSales <sophiabrenda10@gmail.com>
2021-05-06 10:24:59 -0300

    aula11 condiçoes simples

---
+++ aula05/script1.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}KM/h`);
+
+if(vel > 60){  //condiçao simplis
+    console.log(`Voce ultrapasou a velocidade permitida.MULTADO`);
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
---
+++ aula05/script2.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Viviendo em ${pais}`);
+
+if (pais == 'Brasil') {
+    console.log('Voce é Brasileiro');
+}else{
+    console.log('Voce é Estrangeiro');
+}
