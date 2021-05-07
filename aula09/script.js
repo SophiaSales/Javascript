2d152809313aa734096dd045b0e36aab5e0456a9
SophiaSales <sophiabrenda10@gmail.com>
2021-05-07 08:59:35 -0300

    aula13 estrutura de repetiçao

---
+++ aula09/script.js
@@ -0,0 +1,12 @@
+var contador = 1
+
+while (contador <=6) { 
+    console.log(`Passo${contador}`);//estrutura de repetiçao ele vai repetir passos 6vezes
+    contador++ //isso é igual a contador = contador + 1 
+}
+//************************************************************************************
+var c = 1
+do{ //faça a condição a baixo até não ser mais verdadeira
+    console.log(`Passo ${c}`); //estrutura de repetiçao com teste logico em baixo 
+    c++
+}while(c<=6)
