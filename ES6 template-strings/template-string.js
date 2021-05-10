534b55d5d885d88c0983aad659b017b5ae2df7d9
SophiaSales <sophiabrenda10@gmail.com>
2021-05-10 15:40:09 -0300

    template strings

---
+++ ES6 template-strings/template-string.js
@@ -0,0 +1,21 @@
+//Exemplo 1 ******************
+const strMultilinha = `linha1
+linha do meio
+linha2`
+console.log(strMultilinha);
+
+//Exemplo 2 ******************
+const a=10
+const str = `Ola ${a+1} !` //string interpolada siguinifica mistura de expraçao com string
+
+console.log(str);
+
+//Exemplo 3 ******************
+function tag(strings, ...value) {
+    console.log(strings, value);
+    console.log(strings.raw[0]);
+    return 'opa'
+}
+const str = tag`Ola
+${10} mundo ${20}!`
+console.log(str)
