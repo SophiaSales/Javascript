a74c6872f05b9bfafec57779b25dc7879417265b
SophiaSales <sophiabrenda10@gmail.com>
2021-05-11 11:40:49 -0300

    exemplos de expressoes

---
+++ OrigamidJS/expressoes/script.js
@@ -0,0 +1,17 @@
+// Exemplos de expressoes 
+const grupoA = 100;
+const grupoB = 400;
+
+const vencedor  = grupoA > grupoB ? "Grupo A ganhou" : "Grupo B ganhou"
+
+console.log(vencedor);
+
+//Exemplo 2 de expressoes
+
+const quadradoLargura = l => l+l
+
+//Exemplo 3 de expressoes
+
+const grupoAvenceu = grupoA > 150 && "Grupo A venceu"
+
+console.log(grupoAvenceu);
